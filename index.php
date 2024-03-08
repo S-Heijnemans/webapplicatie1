9157c47fba406b99821cd2f76f4dd3513af05431
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2024-03-08 13:40:34 +0100

    einde voor opschonen
    einde van de bende en een schoon begin

--- index.php
+++ index.php
@@ -27,9 +27,50 @@
     </nav>
     <div class='main-page'>
         <div class="img-line-1">
-            <img src="assets/image-landing-1.jpg" height="100px" width="100px" alt="image nt loading">
+            <img class='image-1-home' src="assets/image-landing-1.jpg" height="100%" width="50%" alt="image nt loading">
+            <img class='image-1-home' src="assets/image-landing-3.jpg" height="100%" width="50%" alt="image nt loading">
         </div>
-        <div class="img-line-2"></div>
+        <div class="img-line-2">
+        <img class='image-1-home' src="assets/image-landing-2.jpg" height="100%" width="50%" alt="image nt loading">
+        <img class='image-1-home' src="assets/image-landing-4.jpg" height="100%" width="50%" alt="image nt loading">
+        </div>
+    </div>
+    <div class='tweede-main-block'>
+        <div class='tweede-main-info'>
+            <div class='info-blok-tweede-main'>
+                <div class='info-blokje-linkerkant-tweede-main'>
+                    <div class='tweede-main-info-titel'>
+
+                    </div>
+                    <div class='tweede-main-info-text'>
+                        
+                    </div>
+                </div>
+                <div class='img-blok-tweede-main-rechts'>
+                    <img>
+                </div>
+            </div>
+
+            
+            <div class='info-blok-tweede-main'>
+                <div class='info-blokje-linkerkant-tweede-main'>
+                    <div class='tweede-main-info-titel'>
+
+                    </div>
+                    <div class='tweede-main-info-text'>
+                        
+                    </div>
+                </div>
+                <div class='img-blok-tweede-main-rechts'>
+                    <img>
+                </div>
+            </div>
+
+        </div>
+
+        <div class='tweede-main-opmaak'>
+        </div>
+
     </div>
 </body>
 </html>
