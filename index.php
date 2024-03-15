32dfd40b97549fdaab334b96911157791efb78ec
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2024-03-15 13:42:30 +0100

    main page index en footer
    footer gemaakt en heb een begin gemaakt aan het maken van de main page

--- index.php
+++ index.php
@@ -25,7 +25,59 @@
         </div>
     </nav>
     <div class='main-page'>
-      
+        <div class='opmaakblok-main-page'></div>
+            <div class='main-container'>
+
+               <div class='container-left'>
+                <div class='eerstelaag-main-links'>
+                    <div class='tweedelaag-main-links'>
+                        <div class='titelblok-main-links'>
+                            <a class='titel-OVER_ONS'>OVER ONS </a>
+                        </div>
+                        <div class='textblok-main-links'>
+                            <a class='text-main-links'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet nibh praesent 
+                                tristique magna sit. Sed viverra ipsum nunc aliquet bibendum enim. Amet consectetur adipiscing
+                                 elit ut aliquam purus sit. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Nunc 
+                                 lobortis mattis aliquam faucibus purus in. Molestie a iaculis at erat pellentesque adipiscing 
+                                 commodo. Felis bibendum ut tristique et egestas. Risus at ultrices mi tempus imperdiet nulla 
+                                 malesuada. Rhoncus est pellentesque elit ullamcorper dignissim cras.</a>
+                        </div>
+                    </div>
+                </div>
+               </div>
+
+              <div class='container-right'>
+                <div class='infoblock-1-main-rechts'>
+                    <div class='titel-eerste-infokopje-main-rechts'>
+                        <a class='titel-main-rechts-1'></a>
+                    </div>
+                    <div class='text-eerste-infokopje-main-rechts'>
+                        <a class='text-main-rechts-1'></a>
+                    </div>
+                </div>
+                <div class='opmaakblock-1-main-rechts'>
+                    <div class='opmaakblock-2-main-rechts'>
+                        <img src="" alt="">
+                    </div>
+                </div>
+                <div class='infoblock-2-main-rechts'>
+                <div class='titel-eerste-infokopje-main-rechts'>
+                        <a class='titel-main-rechts-1'></a>
+                    </div>
+                    <div class='text-eerste-infokopje-main-rechts'>
+                        <a class='text-main-rechts-1'></a>
+                    </div>
+                </div>
+
+              </div>
+
+            </div>
+        <div class='opmaakblok-main-page-2'></div>
     </div>
+
+<?php
+    include "pages/footer.php";
+?>
 </body>
 </html>
---
+++ pages/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class='opmaakblok-footer-index'>
+        <div class='opmaakblok-footer-2'></div>
+        </div></div>
+        <div class='logo-footer-block'>
+            <a class='logo-footer'>IJsPaleis</a>
+        </div>
+        <div class='opmaakblok-footer-index-2'>
+           <div class='opmaakblok-footer-2-2'></div>
+        </div>
+    </footer>
